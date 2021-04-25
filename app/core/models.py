910719874fa39d19fe50cb2302837abb570458c4
Xzib <syedzohaibali94@gmail.com>
2021-04-25 13:48:07 +0500

    linting errors removed

--- app/core/models.py
+++ app/core/models.py
@@ -6,32 +6,32 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user""" 
+        """Creates and saves a new user"""
         if not email:
             raise ValueError("User must have an email")
-        user =self.model(email=self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
-        return user 
-    
-    def create_superuser(self,email,password):
+        return user
+
+    def create_superuser(self, email, password):
         """Creates a new superuser"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using= self._db)
+        user.save(using=self._db)
 
         return user
 
 
-class User(AbstractBaseUser,PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default= True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserManager()      
+    objects = UserManager()
 
-    USERNAME_FIELD = 'email'                            
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,7 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successfull(self):
         email = "test@abc.com"
         password = 'Testpass123'
-        user =get_user_model().objects.create_user(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
@@ -23,7 +23,7 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         """Test creating user with no email raise error"""
         with self.assertRaises(ValueError):
-            user = get_user_model().objects.create_user(None,"Testpass123")
+            get_user_model().objects.create_user(None, "Testpass123")
 
     def test_create_new_superuser(self):
         """Test for creating a new superuser"""
